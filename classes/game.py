ca325bb5453efbbdb080a26218e6dc7676d00bb2
bhatnaushad <bhatnaushad@gmail.com>
2018-07-01 11:39:39 +0530

    We and enemy can attact

--- classes/game.py
+++ classes/game.py
@@ -11,17 +11,9 @@ class BColors:
         BOLD = '\033[1m'
         UNDERLINE = '\033[4m'
 
-        def disable(self):
-            self.HEADER = ''
-            self.OKBLUE = ''
-            self.OKGREEN = ''
-            self.WARNING = ''
-            self.FAIL = ''
-            self.ENDC = ''
-
 
 class Person:
-    def __init__(self, hp, mp, atk, df, magic):    # health, magic point, attack, defence, magic
+    def __init__(self, hp, mp, atk, df, magic):    # health, magic point, attack power, defence, magic spell
         self.maxhp = hp    # hp can change, but it shouldn't go beyond the maximum value
         self.hp = hp
         self.maxmp = mp
@@ -35,5 +27,50 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]["damage"] - 5
+        mgh = self.magic[i]["damage"] + 5
+        return random.randrange(mgl, mgh)
 
-    
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp <= 0:
+            self.hp = 0
+        return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i = 1
+        print("Actions")
+        for item in self.actions:
+            print(str(i) + ":" + item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print("Magic")
+        for spell in self.magic:
+            print(str(i) + ":" + spell["name"], "(cost", str(spell["mp"]) + ")")
+            i += 1
+
+
--- main.py
+++ main.py
@@ -1,14 +1,57 @@
 from classes.game import BColors, Person
 
 
-magic = [{"name": "Fire", "Cost": 10, "damage": 60},
-         {"name": "Thunder", "Cost": 10, "damage": 70},
-         {"name": "Blizzard", "Cost": 10, "damage": 80}]
+magic = [{"name": "Fire", "cost": 10, "damage": 100},
+         {"name": "Thunder", "cost": 15, "damage": 120},
+         {"name": "Blizzard", "cost": 10, "damage": 100}]
 
 
 player = Person(400, 90, 60, 30, magic)
 
-print(player.generate_damage())
-print(player.generate_damage())
-print(player.generate_damage())
-print(player.generate_damage())
+# print(player.generate_damage())
+# print(player.generate_damage())
+# print(player.generate_damage())
+# print(player.generate_damage())
+#
+# print(player.generate_spell_damage(0))
+# print(player.generate_spell_damage(2))
+#
+# print(player.take_damage(40))
+# print(player.get_spell_name(2))
+# print(player.get_spell_cost(1))
+# print(player.get_max_hp())
+
+# we will create an enemy now - enemy also is a person
+enemy = Person(600, 70, 50, 20, magic)
+
+running = True
+
+print(BColors.FAIL + BColors.BOLD + "AN ENEMY ATTACK" + BColors.ENDC + " Get ready")
+
+while running:
+    print("=======================")
+    player.choose_action()
+    choice = input("Enter a choice: ")
+    index = int(choice) - 1    # as index is 1 in choose_action() function
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)  # we are attacking the enemy
+        print("you attacked the enemy for = ", dmg, " points of damage. New Enemy Hp = ", enemy.get_hp())
+    elif index == 1:
+        player.choose_magic()
+        # magic_dmg = player.generate_spell_damage(magic_number)
+
+    enemy_choice = 1  # Not understood
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacked you for = ", enemy_dmg, " points. Your New Hp = ", player.get_hp())
+
+    if enemy.get_hp() == 0:
+        print(BColors.OKGREEN + "You won. Hurray! " + BColors.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(BColors.FAIL + " You have been defeated. :( " + BColors.ENDC)
+        running = False
+
+
